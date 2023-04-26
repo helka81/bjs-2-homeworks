c3a54f5d68b45789d9717fc036feec3a694a8e6e
Елена Бархатова <hell_sun.net@inbox.ru>
2023-04-26 15:05:09 +0300

    Обе задачи выполнены

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	const discriminant = b ** 2 - 4 * a * c;
+	let arr = [];
+	if (discriminant < 0) {
+		return arr;
+	 } else if (discriminant === 0) {
+		arr.push(-b / (2 * a));
+		return arr;
+	} else {
+		arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+		arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+		return arr;
+	}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	if (typeof percent !== 'number' || typeof contribution !== 'number' || typeof amount !== 'number' || typeof countMonths !== 'number') {
+		return false;
+	}
+  const monthlyInterestRate = percent / 100 / 12;
+  const bodyOfCredit = amount - contribution;
+  const monthlyPayment = bodyOfCredit * (monthlyInterestRate + monthlyInterestRate / ((1 + monthlyInterestRate) ** countMonths - 1));
+  const totalAmount = parseFloat((monthlyPayment * countMonths).toFixed(2));
+  return totalAmount;
 }
