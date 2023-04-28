173d7546d9976d28b1760bfb0dfc56c4519b868a
Елена Бархатова <hell_sun.net@inbox.ru>
2023-04-28 15:53:39 +0300

    Второе задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,18 +1,34 @@
 "use strict";
 
+// function solveEquation(a, b, c) {
+// 	const discriminant = b ** 2 - 4 * a * c;
+// 	let arr = [];
+// 	if (discriminant < 0) {
+// 		return arr;
+// 	 } else if (discriminant === 0) {
+// 		arr.push(-b / (2 * a));
+// 		return arr;
+// 	} else {
+// 		arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+// 		arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+// 		return arr;
+// 	}
+// }
+
 function solveEquation(a, b, c) {
-	const discriminant = b ** 2 - 4 * a * c;
-	let arr = [];
-	if (discriminant < 0) {
-		return arr;
-	 } else if (discriminant === 0) {
-		arr.push(-b / (2 * a));
-		return arr;
-	} else {
-		arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
-		arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
-		return arr;
-	}
+  const discriminant = b ** 2 - 4 * a * c;
+  let arr = [];
+  
+  if (discriminant > 0) {
+    const x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    const x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    
+    arr.push(x1, x2);
+  } else if (discriminant === 0) {
+    arr.push(-b / (2 * a));
+  }
+  
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,72 @@
-function getArrayParams(...arr) {
+'use strict';
 
-  return { min: min, max: max, avg: avg };
+function getArrayParams(...arr) {
+	let max = Math.max(...arr);
+	let min = Math.min(...arr);
+	let sum = arr.reduce((sum, current) => sum + current);
+	let avg = +((sum / arr.length).toFixed(2));
+	return {
+		min: min,
+		max: max,
+		avg: avg
+	};
 }
 
 function summElementsWorker(...arr) {
-
+	if (arr.length === 0) {
+		return 0;
+	}
+	return arr.reduce((sum, current) => sum + current);
 }
 
 function differenceMaxMinWorker(...arr) {
-
+	if (arr.length === 0) {
+		return 0;
+	}
+	return Math.max(...arr) - Math.min(...arr);
 }
 
 function differenceEvenOddWorker(...arr) {
-
+	if (arr.length === 0) {
+		return 0;
+	}
+	let sumEvenElement = 0;
+	let sumOddElement = 0;
+	for (let i = 0; i < arr.length; i += 1) {
+		if (arr[i] % 2 === 0) {
+			sumEvenElement += arr[i];
+		} else {
+			sumOddElement += arr[i];
+		}
+	}
+	return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
-
+	if (arr.length === 0) {
+		return 0;
+	}
+	let sumEvenElement = 0;
+	let countEvenElement = 0;
+	for (let i = 0; i < arr.length; i += 1) {
+		if (arr[i] % 2 === 0) {
+			sumEvenElement += arr[i];
+			countEvenElement += 1;
+		}
+	}
+	if (countEvenElement === 0) {
+		return 0;
+	}
+	return sumEvenElement / countEvenElement;
 }
 
-function makeWork (arrOfArr, func) {
-
+function makeWork(arrOfArr, func) {
+	let maxWorkerResult = func(...arrOfArr[0]);
+	for (let i = 0; i < arrOfArr.length; i += 1) {
+		const result = func(...arrOfArr[i]);
+		if (result > maxWorkerResult) {
+			maxWorkerResult = result;
+		}
+	}
+	return maxWorkerResult;
 }
